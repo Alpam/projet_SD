ad379b9eacf022459863d104700877fd0a337946
Paul Robin <paul.robin@etu.unistra.fr>
2018-03-30 18:01:24 +0200

    repercussion des ajustement de participant et ajustement des arguments d'entrer pour rendre l'instanciation d'objet plus personnalisable

--- p_manager.py
+++ p_manager.py
@@ -21,43 +21,48 @@ from queue import *
 import sys
 
 class Participant(Thread):
-    def __init__(self, queue, dest, host, mtrcl, sem, l_prt):
+    def __init__(self, queue, dest, host, mtrcl, sem, l_prt, lf, mm_tr, mm_slp, test):
         Thread.__init__(self)
         self.q = queue
         self.dest = dest
         self.address= [mtrcl,host]
         self.sem = sem
         self.l_prt = l_prt
+        self.lf = lf
+        self.mm_tr = mm_tr
+        self.mm_slp = mm_slp
+        self.test = test[0]
+        self.t_slp = test[1]
 
     def run(self):
         counter = 0
         self.dest.append(self.address)
         message = mg.m_builder(counter,self.address,'C')
         self.q.put(message)
-        life = pn.p_life(3,6)
-        test = 2
+        life = pn.p_random(self.lf)
+        ctest = self.test
         while(life):
             counter += 1
             life -= 1
-            v = pn.p_trans([1,10])
-            pn.p_slp([1,20])
+            v = pn.p_random(self.mm_tr)
+            pn.p_slp(self.mm_slp)
             dstnt = pn.p_dstnt(self.dest,self.address)
             #bout de code pour verifier qu'il y ai bien une destination
             #si il n'y en a pas att un petit temps, et reessaye
             #au bout d'un certain nombre d'essai non concluant successif
             #le participant s'arretera
             if(not(dstnt)):
-                if(not(test)):
-                    message = mg.m_builder(counter,self.address,'E')
+                if(not(ctest)):
+                    message = mg.m_builder(counter,self.address,'E',None,self.test)
                     self.q.put(message)
                     break
                 else :
-                    test -= 1
-                    pn.p_slp([2,2])
+                    ctest -= 1
+                    pn.p_slp(self.t_slp)
                     counter -= 1
                     life += 1
                     continue
-            test = 2
+            ctest = self.test
             message = mg.m_builder(counter,self.address,'T',dstnt,v)
             self.q.put(message)
         counter += 1
@@ -70,13 +75,18 @@ class Participant(Thread):
 
 class Manager(Thread):
     #nb : float('inf') represente l'infinit, marche aussi pour des comparaisons d'int
-    def __init__(self, queue, dest, host, max_p=50, tot_p=float('inf')):
+    def __init__(self, queue, dest, host, max_p = 50, tot_p = float('inf'), \
+                 lf = 3, mm_tr = 2, mm_slp = 0.05, test = [2,0.025]):
         Thread.__init__(self)
         self.q = queue
         self.dest = dest
         self.h = host
         self.tot_p = tot_p
+        self.lf = lf
+        self.mm_tr = mm_tr
+        self.mm_slp = mm_slp
         self.sem = Semaphore(max_p)
+        self.t = test
         #use semaphore to stop the manager thread to create more 
         #participants than expect
 
@@ -85,7 +95,16 @@ class Manager(Thread):
         cur_prts = []
         while(nb_prtcpt < self.tot_p):
             self.sem.acquire()
-            cur_prts.append(Participant(self.q, self.dest, self.h, nb_prtcpt, self.sem, cur_prts))
+            cur_prts.append(Participant(self.q,     \
+                                        self.dest,  \
+                                        self.h,     \
+                                        nb_prtcpt,  \
+                                        self.sem,   \
+                                        cur_prts,   \
+                                        self.lf,    \
+                                        self.mm_tr, \
+                                        self.mm_slp,\
+                                        self.t))
             cur_prts[-1].start()
             nb_prtcpt += 1
         return
