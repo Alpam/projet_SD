133ea75f4dabae1dd79fcc77681725d7ce6b2bb4
Paul Robin <paul.robin@etu.unistra.fr>
2018-04-14 20:40:48 +0200

    modifcation message- creation classe Operation + methode de traduction et d'affichage

--- bloc_node/participant_node/m_operation/ope_generator.py
+++ bloc_node/participant_node/m_operation/ope_generator.py
@@ -14,101 +14,85 @@
 
 #!/usr/bin/python3
 
-def m_builder(nb_op,exp,tra,dest=None,value=None):
-    message = str(nb_op) + "OP" + str(exp[0]) + "OB" + str(exp[1])+ "%" + tra
-    if(tra == 'T'):
-        message = message + "DP" + str(dest[0]) + "DB" + str(dest[1]) \
-                + "%" + str(value)
-    if(tra == 'E'):
-        message = message + str(value)
-    if(tra == 'G'):
-        message = message + str(dest) + '%' + str(value)
-    return message
+class Operation:
+#il faudrait rajouter des exceptions pour gérer les strings/args invalides
 
-def m_reader(string):
-    pos_eo = string.find("%")
-    if(pos_eo == -1):
-        return None
-    pos_op = string.find("OP")
-    if(pos_op == -1):
-        return None
-    pos_ob = string.find("OB")
-    if(pos_ob == -1):
-        return None
-    mes_t = string[pos_eo+1]
-    num_o = string[:pos_op]
-    org_p = string[pos_op+2:pos_ob]
-    org_b = string[pos_ob+2:pos_eo]
-    dst_p = None
-    dst_b = None
-    val   = None
-    if(mes_t == "T"):
-        pos_dp = string.find("DP")
-        if(pos_dp == -1):
-            return None
-        pos_db = string.find("DB")
-        if(pos_db == -1):
-            return None
-        pos_ed = string.rfind("%")
-        if(pos_ed == -1):
-            return None
-        dst_p = string[pos_dp+2:pos_db]
-        dst_b = string[pos_db+2:pos_ed]
-        val   = string[pos_ed+1:]
-    elif(mes_t == 'C' or mes_t == 'D' or mes_t == 'E'):
-        if(mes_t == 'E'):
-            dst_p = string[pos_eo+2:]
-    elif(mes_t == 'G'):
-        pos_ed = string.rfind("%")
-        if(pos_ed == -1):
-            return None
-        dst_p = string[pos_eo+2:pos_ed]
-        dst_b = string[pos_ed+1:]
-    else :
-        return None
-    return mes_t,num_o,org_p,org_b,dst_p,dst_b,val
+    def __init__(self,mt=None,i=None,org=None,dst=None,mh=None, \
+                nc=None,vl=None,tm=None):
 
-def m_type(string):
-    i = string.find('%')
-    return string[i+1]
+            self.m_type = mt
+            self.origin = org
+            self.dstntn = dst
+            self.m_hash = mh
+            self.nonce  = nc
+            self.value  = vl
+            self.ident  = i
+            self.transmitter = tm
 
-def m_origin(string):
-    pos_op = string.find("OP")
-    if(pos_op == -1):
-        return None
-    pos_ob = string.find("OB")
-    if(pos_ob == -1):
-        return None
-    pos_eo = string.find("%")
-    if(pos_eo == -1):
-        return None
-    op = string[pos_op+2:pos_ob]
-    ob = string[pos_ob+2:pos_eo]
-    if(op.isdigit()):
-        return int(op),int(ob)
-    else:
-        return int(ob)
+    def str_injection(self, string):
+        self.m_type = None
+        self.origin = None
+        self.dstntn = None
+        self.m_hash = None
+        self.nonce  = None
+        self.value  = None
+        self.ident  = None
+        self.transmitter = None
+        foo = []
+        tmp = ""
+        for c in string :
+            if(c!='%'):
+                tmp += c
+            else:
+                foo.append(tmp)
+                tmp = ""
+        foo.append(tmp)
+        self.m_type = foo[0]
+        self.ident  = int(foo[1])
+        if(self.m_type == 'T'):
+            self.origin = [int(foo[2]),int(foo[3])]
+            self.dstntn = [int(foo[4]),int(foo[5])]
+            self.value  = int(foo[6])
+        elif(self.m_type == 'G'):
+            self.origin = int(foo[2])
+            self.m_hash = foo[3]
+            self.nonce  = foo[4]
+            i = 5
+            while(i<len(foo)):
+                self.nonce += '%' + foo[i]
+                i += 1
+        elif(self.m_type == 'C' or self.m_type == 'D' or self.m_type == 'E'):
+            self.origin = [int(foo[2]),int(foo[3])]
+            if(self.m_type == 'E'):
+                self.value = [int(foo[4])]
+        return
 
-def m_destination(string):
-    if(m_type(string)!='T'):
-        return None
-    pos_dp = string.find("DP")
-    if(pos_dp == -1):
-        return None
-    pos_db = string.find("DB")
-    print(pos_db)
-    if(pos_db == -1):
-        return None
-    pos_eo = string.rfind("%")
-    print(pos_eo)
-    if(pos_eo == -1):
-        return None
-    dp = string[pos_dp+2:pos_db]
-    db = string[pos_db+2:pos_eo]
-    print(string)
-    print(dp)
-    print(db)
-    if(dp.isdigit()):
-        return int(dp),int(db)
-    else:
-        return int(db)
+    def __str__(self):
+        rtr = ""
+        rtr += "Type :" + self.m_type + " Id :" + str(self.ident)
+        if(self.m_type == 'T'):
+            rtr += " Origine :" + str(self.origin[0]) + "-" + str(self.origin[1]) +     \
+                   " Destination :" + str(self.dstntn[0]) + "-" + str(self.dstntn[1]) + \
+                   " Valeur :"+ str(self.value)
+        elif(self.m_type == 'G'):
+            rtr += " Origine :" + str(self.origin) + "\nHash :" + self.m_hash + "\nNonce:" + self.nonce
+        elif(self.m_type == 'C' or self.m_type == 'D' or self.m_type == 'E'):
+            rtr += " Origine :" + str(self.origin[0]) + "-" + str(self.origin[1])
+            if(self.m_type == 'E'):
+                rtr += " Nombres de test avant arrêt :" + str(self.value)
+        return rtr
+
+    def translation(self):
+        rtr = ""
+        rtr += self.m_type + '%' + str(self.ident) + '%'
+        if(self.m_type == 'T'):
+            rtr += str(self.origin[0]) + '%' + str(self.origin[1]) + '%' + \
+                   str(self.dstntn[0]) + '%' + str(self.dstntn[1]) + '%' + str(self.value)
+        elif(self.m_type == 'G'):
+            rtr += str(self.origin) + '%' + self.m_hash + '%' + self.nonce
+        elif(self.m_type == 'C' or self.m_type == 'D' or self.m_type == 'E'):
+            rtr += str(self.origin[0]) + '%' + str(self.origin[1])
+            if(self.m_type == 'E'):
+                rtr += '%' + str(self.value)
+        return rtr
+
